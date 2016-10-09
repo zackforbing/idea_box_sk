471b5fdab975170c63d0e35967ea7a5f5ec27772
Zack Forbing <zackforbing@gmail.com>
2016-10-09 17:38:05 -0600

    more controller tests

--- test/controllers/api/v1/ideas_controller_test.rb
+++ test/controllers/api/v1/ideas_controller_test.rb
@@ -13,4 +13,20 @@ class Api::V1::IdeasControllerTest < ActionController::TestCase
 
     assert_kind_of Array, json_response
   end
+
+  test 'index returns the correct number of ideas' do
+    get :index, format: :json
+
+    assert_equal Idea.count, json_response.count
+  end
+
+  test 'index contains ideas with correct attributes' do
+    get :index, format: :json
+
+    json_response.each do |idea|
+      assert idea['title']
+      assert idea['body']
+      assert idea['quality']
+    end
+  end
 end
